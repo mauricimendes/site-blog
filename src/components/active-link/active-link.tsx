2cec0632540b5a6e3c947a2dfc66d08d8b4c8cc7
mauricimendes <mauricimendes.14@gmail.com>
2025-12-04 01:29:58 -0300

    first commit

---
+++ src/components/active-link/active-link.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils"
+import Link, { LinkProps } from "next/link"
+import { useRouter } from "next/router"
+
+type ActiveLink = {
+  children: React.ReactNode
+} & LinkProps
+
+export const ActiveLink = ({ children, href, ...rest }: ActiveLink) => {
+  const router = useRouter()
+  const isCurrentPath = router.asPath === href || router.asPath === rest.as
+
+  return (
+    <Link href={href} className={cn('text-action-sm transition-colors hover:text-blue-200',
+      isCurrentPath ? 'text-blue-500' : 'text-gray-100')}
+    >
+      {children}
+    </Link>
+  )
+}
