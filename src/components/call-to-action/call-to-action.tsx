480808f8cf05b766d502b0223359706a7d090f4d
mauricimendes <mauricimendes.14@gmail.com>
2025-12-04 14:39:12 -0300

    call to action section, customer story and support

---
+++ src/components/call-to-action/call-to-action.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+import { Button } from "../ui/button"
+import { ArrowRight, Store } from "lucide-react"
+
+export const CallToAction = () => {
+  return (
+    <section className="py-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div className="container">
+
+        <div className="flex flex-col items-center gap-6 text-center">
+          <div className="p-4 bg-cyan-300 w-fit rounded-full">
+            <Store className="text-cyan-100" />
+          </div>
+
+          <h2 className="text-gray-100 text-balance text-heading-xl font-sans">Crie uma loja online e inice suas vendas ainda hoje</h2>
+          <Button
+            asChild
+            variant="primary"
+            className="mt-6"
+          >
+            <Link href="/criar-loja">
+              Criar loja grátis
+              <ArrowRight />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  )
+}
